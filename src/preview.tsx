a49a1af812decf1eff88d68205519e2c58a0916d
Francisco Lopez <franciscolopto@gmail.com>
2023-01-25 17:50:40 +0100

    refactor preview

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighligter } from 'react-syntax-highlighter';
-import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import ReactMarkdown from 'react-markdown';
 import './preview.css';
 import './github-markdown.css';
@@ -16,24 +14,6 @@ export const Preview: React.FC<Props> = (props) => {
       <ReactMarkdown
         children={props.doc}
         remarkPlugins={[remarkGfm]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighligter
-              children={String(children).replace(/\n$/, '')}
-              style={prism}
-              language={match[1]}
-              PreTag='div'
-              {...props}
-              />
-            ):(
-              <code className={className} {...props}>
-                {children}
-              </code>
-            )
-          },
-        }}
       />
     </div>
   );
