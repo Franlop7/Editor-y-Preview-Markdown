2836bfd4df95937a11404dfc63c4efef512ac6f8
Francisco Lopez <franciscolopto@gmail.com>
2023-02-22 20:03:43 +0100

    update codemirror and styles

--- src/codemirror.tsx
+++ src/codemirror.tsx
@@ -1,12 +1,20 @@
 import type React from 'react';
 import { useEffect, useState, useRef } from 'react';
 import { EditorState } from '@codemirror/state';
-import { defaultKeymap } from '@codemirror/commands';
-import { EditorView, keymap } from '@codemirror/view';
+import { EditorView } from '@codemirror/view';
 import { languages } from '@codemirror/language-data';
-import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { markdown } from '@codemirror/lang-markdown';
 import { oneDark } from '@codemirror/theme-one-dark';
 import { basicSetup } from 'codemirror';
+import { tags } from '@lezer/highlight';
+import { syntaxHighlighting, HighlightStyle } from "@codemirror/language";
+import classes from './styles.module.css';
+
+const stylesEditor = HighlightStyle.define([
+  { tag: tags.heading1, class: classes.h1},
+  { tag: tags.heading2, class: classes.h2},
+  { tag: tags.heading3,class: classes.h3},
+]);
 
 interface Props {
   initialDoc: string;
@@ -26,12 +34,10 @@ export const codeMirror = <T extends Element>(
       doc: props.initialDoc,
       extensions: [
         basicSetup,
-        keymap.of(defaultKeymap),
         markdown({
-          base: markdownLanguage,
           codeLanguages: languages,
-          addKeymap: true,
         }),
+        syntaxHighlighting(stylesEditor),
         oneDark,
         EditorView.lineWrapping,
         EditorView.updateListener.of((update) => {
